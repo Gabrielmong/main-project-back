1d7716a8e30fc817b7ddf2d56b40c46211b556b6
Esteban Salguero <jesalguero6v@gmail.com>
2022-08-02 17:50:22 -0600

    chore: changes

--- server/libs/dbconfig.js
+++ server/libs/dbconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     user          : process.env.NODE_ORACLEDB_USER || "VSCODE",
     password      : process.env.NODE_ORACLEDB_PASSWORD || "ORACLE",
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orclpdb"
+    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orcl"
 };
   
 
