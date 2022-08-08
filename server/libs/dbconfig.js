c7b833098eb396434b194a3815e7f9fcb059894c
Gabriel Monge <gabriel.monge.lizano@gmail.com>
2022-08-08 10:07:50 -0600

    .env file

--- server/libs/dbconfig.js
+++ server/libs/dbconfig.js
@@ -1,8 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
     user          : process.env.NODE_ORACLEDB_USER || "VSCODE",
     password      : process.env.NODE_ORACLEDB_PASSWORD || "ORACLE",
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orcl"
+    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || process.env.CONN_STRING,
 };
-  
-
-//aaa
